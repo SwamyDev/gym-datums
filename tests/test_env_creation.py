e8083ea528de7114ad898a1008b93211d4c74f31
SwamyDev <b.raml@gmx.at>
2019-11-03 14:47:55 +0100

    Fix broken creation tests

--- tests/test_env_creation.py
+++ tests/test_env_creation.py
@@ -1,11 +1,11 @@
 import gym
 
 
-def test_creating_environment():
-    env = gym.make('gym_datums:portfolio-v0')
+def test_creating_environment(datums):
+    env = gym.make('gym_datums:portfolio-v0', datums=datums.get_list())
     assert env
 
 
-def test_pass_configuration_to_environment():
-    env = gym.make('gym_datums:portfolio-v0', window_size=10)
+def test_pass_configuration_to_environment(datums):
+    env = gym.make('gym_datums:portfolio-v0', datums=datums.get_list(), window_size=10)
     assert env.window_size == 10
